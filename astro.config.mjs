972481da73c55d1a3be1a0f6fa4e6c89de9534f6
zayez <zayezuno@protonmail.com>
2024-05-13 03:33:27 -0300

    Add react and giscus

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config'
-
 import mdx from '@astrojs/mdx'
+import react from '@astrojs/react'
 
 export default defineConfig({
-  site: 'https://zayez.github.io',
-
-  integrations: [mdx()],
+  site: 'https://zayez.xyz',
+  integrations: [mdx(), react()],
   base: '/',
   build: {
     assetsPrefix: '/',
